e901c662494a09d860eceb6aeb5294bb6a5a774b
LuisRodrigo <luisbacajol123@gmail.com>
2024-06-21 00:01:11 -0600

    Se corrigieron conflictos en head

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
+
 import { useRoutes } from "react-router-dom"
 import { Toaster } from "react-hot-toast"
 import routes from "./routes.jsx"
 
 export const App = () => {
-    let element = useRoutes( routes );
+    let element = useRoutes(routes);
     return (
         <>
             {element}
@@ -14,25 +14,4 @@ export const App = () => {
 }
 
 export default App;
-=======
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import React from 'react'
-import Login from './pages/login/Login.jsx'
-import Register from './pages/register/Register.jsx'
-import HomePage from './pages/homePage/HomePage.jsx'
 
-function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login></Login>} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/homePage" element={<HomePage />} />
-
-            </Routes>
-        </BrowserRouter>
-    )
-}
-
-export default App
->>>>>>> 91fd46b7f09e2c9ed2cd5c442645b5620a4c3c63
--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot( document.getElementById( 'root' ) ).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App></App>
   </BrowserRouter>,
-=======
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App></App>
-  </React.StrictMode>,
->>>>>>> 91fd46b7f09e2c9ed2cd5c442645b5620a4c3c63
 )
