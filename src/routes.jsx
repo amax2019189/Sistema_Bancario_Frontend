af576b9d5fff913e5aa34edf350c89aebab4b5b0
Dsanchez-2018471 <dsanchez-2018471@kinal.edu.gt>
2024-06-20 23:34:43 -0600

    se agregaron importaciones en routes sobre transfer

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,8 @@ import { Register } from '../src/pages/register/Register.jsx';
 import { AuthPage } from '../src/pages/auth'
 import HomePage from './pages/homePage/HomePage.jsx'
 import Prueba from './pages/prueba/prueba.jsx';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import TransferPage from './components/TransferPage';
 
 // Routes
 const routes = [
@@ -12,3 +14,15 @@ const routes = [
 ];
 
 export default routes;
+
+// Transfer
+function App() {
+    return (
+      <Router>
+        <Switch>
+          {/* Otras rutas */}
+          <Route path="/transfer" component={TransferPage} />
+        </Switch>
+      </Router>
+    );
+  }
