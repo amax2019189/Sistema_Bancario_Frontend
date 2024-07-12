4a63a6555c94df24dd98603dae0939a6db0bf8d9
lvaquin2022300 <lvaquin-2022300@kinal.edu.gt>
2024-07-12 08:09:34 -0600

    implement tailwind and component

--- src/components/deposits/ParentDepositComponent.jsx
+++ src/components/deposits/ParentDepositComponent.jsx
@@ -9,65 +9,63 @@ import HeadPage from "../HeadPage";
 
 
 export const ParentComponent = () => {
-    const [showDepositForm, setShowDepositForm] = useState( false );
-    const [showEditForm, setShowEditForm] = useState( false );
-    const [showReverseForm, setShowReverseForm] = useState( false )
-    const [showServiceForm, setShowServiceForm] = useState( false );
+    const [showDepositForm, setShowDepositForm] = useState(false);
+    const [showEditForm, setShowEditForm] = useState(false);
+    const [showReverseForm, setShowReverseForm] = useState(false)
+    const [showServiceForm, setShowServiceForm] = useState(false);
 
     const toggleDepositForm = () => {
-        setShowDepositForm( !showDepositForm );
-        setShowEditForm( false );
-        setShowReverseForm( false )
-        setShowServiceForm( false )
+        setShowDepositForm(!showDepositForm);
+        setShowEditForm(false);
+        setShowReverseForm(false)
+        setShowServiceForm(false)
     };
 
     const toggleEditForm = () => {
-        setShowEditForm( !showEditForm );
-        setShowDepositForm( false );
-        setShowReverseForm( false )
-        setShowServiceForm( false )
+        setShowEditForm(!showEditForm);
+        setShowDepositForm(false);
+        setShowReverseForm(false)
+        setShowServiceForm(false)
     };
 
     const toggleReverseForm = () => {
-        setShowReverseForm( !showReverseForm );
-        setShowDepositForm( false );
-        setShowEditForm( false );
-        setShowServiceForm( false )
+        setShowReverseForm(!showReverseForm);
+        setShowDepositForm(false);
+        setShowEditForm(false);
+        setShowServiceForm(false)
     };
 
     //toggle Service register
     const toggleServiceForm = () => {
-        setShowServiceForm( !showServiceForm )
-        setShowDepositForm( false );
-        setShowEditForm( false );
-        setShowReverseForm( false )
+        setShowServiceForm(!showServiceForm)
+        setShowDepositForm(false);
+        setShowEditForm(false);
+        setShowReverseForm(false)
     }
 
-    const [userData, setUserData] = useState( null );
+    const [userData, setUserData] = useState(null);
 
-    useEffect( () => {
+    useEffect(() => {
         // Función para obtener los datos del usuario desde localStorage
         const getUserDataFromLocalStorage = () => {
-            const user = localStorage.getItem( "user" );
-            if ( user ) {
-                const parsedUser = JSON.parse( user );
-                setUserData( parsedUser );
+            const user = localStorage.getItem("user");
+            if (user) {
+                const parsedUser = JSON.parse(user);
+                setUserData(parsedUser);
             }
         };
 
         getUserDataFromLocalStorage();
-    }, [] );
+    }, []);
 
     return (
         <>
             <Sidebar />
-            <div className=" ml-80 p-none ">
+            <div className="ml-80 p-none">
                 <HeadPage />
             </div>
-
-            <div className=" w-auto flex items-center gap-7 ml-80 p-7 ">
-
-                <div className="card bg-white rounded-lg shadow-md p-6 border border-gray-200 w-full max-w-2xl">
+            <div className="w-[80rem] h-full grid grid-cols-1 md:grid-cols-2 gap-9 ml-[30rem] p-7">
+                <div className="card bg-white rounded-lg shadow-md p-6 border border-gray-200 w-full">
                     <img
                         src="https://plus.unsplash.com/premium_photo-1680792417523-156a3568162e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                         alt="Imagen de Depósito"
@@ -82,7 +80,7 @@ export const ParentComponent = () => {
                     {showDepositForm && <DepositForm />}
                 </div>
 
-                <div className="card bg-white rounded-lg shadow-md p-6 border border-gray-200 w-full max-w-2xl">
+                <div className="card bg-white rounded-lg shadow-md p-6 border border-gray-200 w-full">
                     <img
                         src="https://images.unsplash.com/photo-1601597111158-2fceff292cdc?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                         alt="Imagen de Edición"
@@ -97,7 +95,7 @@ export const ParentComponent = () => {
                     {showEditForm && <EditDepositForm />}
                 </div>
 
-                <div className="card bg-white rounded-lg shadow-md p-6 border border-gray-200 w-full max-w-2xl">
+                <div className="card bg-white rounded-lg shadow-md p-6 border border-gray-200 w-full">
                     <img
                         src="https://images.unsplash.com/photo-1551260627-fd1b6daa6224?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                         alt="Imagen de Reversión"
@@ -112,22 +110,22 @@ export const ParentComponent = () => {
                     {showReverseForm && <ReverseDepositForm />}
                 </div>
 
-                <div className="card bg-white rounded-lg shadow-md p-6 border border-gray-200 w-full max-w-2xl">
+                <div className="card bg-white rounded-lg shadow-md p-6 border border-gray-200 w-full">
                     <img
                         src="https://st3.depositphotos.com/1007283/15266/i/450/depositphotos_152667528-stock-photo-registration-printed-on-rubber-stamp.jpg"
                         alt="Imagen de Registro de Servicio"
                         className="w-full h-48 object-cover rounded-t-lg mb-4"
                     />
                     <button
-                        className="mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 w-full"
+                        className="mb-4 px-4 py-2 bg-gray-900 text-white rounded hover:bg-blue-700 w-full"
                         onClick={toggleServiceForm}
                     >
                         {showServiceForm ? 'Ocultar Formulario de Registro de Servicio' : 'Mostrar Formulario de Registro de Servicio'}
                     </button>
                     {showServiceForm && <RegisterServiceForm />}
                 </div>
-
             </div>
         </>
+
     );
 };
