9113d5d5b422a6ae5a3e59c04408eaf83c362001
lvaquin2022300 <lvaquin-2022300@kinal.edu.gt>
2024-07-12 01:02:33 -0600

    Se agrego estado de account

--- src/routes.jsx
+++ src/routes.jsx
@@ -10,7 +10,7 @@ import AccountDetails from './components/account/account.jsx';
 import Convertidor from './components/convertidor/Convertidor.jsx';
 import { PayService } from "./components/services/ServicesForm.jsx";
 import { PaidServices } from './components/services/ServicesView.jsx';
-import { AccountSummary } from './components/accountBalance/AccountBalance.jsx';
+import { AccountBalance } from './components/accountBalance/AccountBalance.jsx';
 import EditUser from './components/account/updateAccount.jsx';
 import { RegisterServiceForm } from '../src/pages/registerService/registerServiceForm.jsx';
 import { CreateAccountForm } from './components/accounts/CreateAccount.jsx'
@@ -31,7 +31,6 @@ const routes = [
   { path: '/services', element: <PayService /> },
   { path: '/accountBalance', element: <AccountBalance /> },
   { path: '/registerService', element: <RegisterServiceForm /> },
-  { path: '/accountBalance', element: <AccountSummary /> },
   { path: '/loans', element: <loanParentComponent /> },
 
 ];
